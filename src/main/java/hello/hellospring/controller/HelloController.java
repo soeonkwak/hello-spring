8ce1049bdd608610da046139867c20c034f75351
Soeon <rhkrthdjs@naver.com>
2022-09-09 01:33:33 +0900

    what is @responseBody

--- src/main/java/hello/hellospring/controller/HelloController.java
+++ src/main/java/hello/hellospring/controller/HelloController.java
@@ -2,23 +2,60 @@ package hello.hellospring.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 public class HelloController {
 
+    //controller mapping 예제
     @GetMapping("hello") //http의 get/post의 get. 임의의 url에 쳐서 들어오는 방법이 get 방식.
     public String hello(Model model){   //MVC의 M. spring이 Model 이라는 걸 만들어서 넣어주는 거.
         model.addAttribute("data", "hello!!");
         return "hello"; //templates 폴더 아래에서 리턴 문자열이랑 일치하는 화면 찾아서 랜더링(model 들구감)
     }
 
-    @GetMapping("Hello-mvc")
+    //MVC 예제
+    @GetMapping("hello-mvc")
     public String helloMvc(@RequestParam(value = "name", required = true) String name, Model model){ //model도 받아서 넘겨줘야함. model에 데이터 담어서 화면으로 넘겨주면 화면에서 받아서 렌더링할 때 씀.
         model.addAttribute("name", name);
         return "hello-template";
+    }
+
+
+    /**
+     * API 예제
+     * 1. 메소드위에 @ResponseBody 어노테이션이 붙으면 스프링 컨테이너에서 viewResolver로 넘기지 않고 HttpMessageConverter한테 보냄
+     * 2. 반환 타입이 String 형이면 그냥 문자 그대로 넘김.
+     * 3. 반환 타입이 객체다? 그럼 객체를 json 데이터 형식으로 바꿔서 넘김. (spring-boot 에서는 jackson 라이브러리가 기본 탑재)
+     */
+    //API 예제 1-1 (string)
+    @GetMapping("hello-string")
+    @ResponseBody //http의 body 부에 return 데이터를 직접 넣어주겠다.
+    public String helloString(@RequestParam(value = "name", required = true) String name){ //model도 받아서 넘겨줘야함. model에 데이터 담어서 화면으로 넘겨주면 화면에서 받아서 렌더링할 때 씀.
+        return "hello " + name;
+    }
+
+    //API 예제 1-2 (Object)
+    @GetMapping("hello-api")
+    @ResponseBody //http의 body 부에 return 데이터를 직접 넣어주겠다.
+    public Hello helloApi(@RequestParam(value = "name", required = true) String name){ //model도 받아서 넘겨줘야함. model에 데이터 담어서 화면으로 넘겨주면 화면에서 받아서 렌더링할 때 씀.
+        Hello hello = new Hello();
+        hello.setName(name);
+
+        return hello;
+    }
+
+
+    static class Hello{
+        private String name;
+
+        //name이라는 멤버변수가 private로 선언되어있기 때문에 외부에서 바로 접근을 못함. 그래서 getter, setter를 통해서 접근해야함.
+        public String getName() {
+            return name;
+        }
+        public void setName(String name) {
+            this.name = name;
+        }
     }
 
 }
