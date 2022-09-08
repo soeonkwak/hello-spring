9a2ad985db0c970b3fe4b5ef23870e8bc682d955
Soeon <rhkrthdjs@naver.com>
2022-09-08 20:07:53 +0900

    mvc 기초

--- src/main/java/hello/hellospring/controller/HelloController.java
+++ src/main/java/hello/hellospring/controller/HelloController.java
@@ -3,6 +3,8 @@ package hello.hellospring.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class HelloController {
@@ -11,6 +13,12 @@ public class HelloController {
     public String hello(Model model){   //MVC의 M. spring이 Model 이라는 걸 만들어서 넣어주는 거.
         model.addAttribute("data", "hello!!");
         return "hello"; //templates 폴더 아래에서 리턴 문자열이랑 일치하는 화면 찾아서 랜더링(model 들구감)
+    }
+
+    @GetMapping("Hello-mvc")
+    public String helloMvc(@RequestParam(value = "name", required = true) String name, Model model){ //model도 받아서 넘겨줘야함. model에 데이터 담어서 화면으로 넘겨주면 화면에서 받아서 렌더링할 때 씀.
+        model.addAttribute("name", name);
+        return "hello-template";
     }
 
 }
